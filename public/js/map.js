c4acc96debb50b77052276f75936cf9bf81f1789
Roger Howard <rogerhoward@gmail.com>
2016-02-08 23:03:25 -0800

    Add fullscreen control

--- public/js/map.js
+++ public/js/map.js
@@ -2,10 +2,14 @@
 console.log('map init start');
 
 L.mapbox.accessToken = 'pk.eyJ1Ijoicm9nZXJob3dhcmQiLCJhIjoiY2lrOXlnZHFvMGc5ZnY0a3ViMHkyYTE0dyJ9.CWAOOChPtxviw8fVB0R1mQ';
-var map = L.mapbox.map('map','mapbox.comic').setView([33.793418, -118.153740], 13).on('ready', function() {
+var map = L.mapbox.map('map','mapbox.comic')
+	.setView([33.793418, -118.153740], 13)
+	.on('ready', function() {
         new L.Control.MiniMap(L.mapbox.tileLayer('mapbox.streets'))
             .addTo(map);
-    });
+});
+
+L.control.fullscreen().addTo(map);
 
 console.log('map init end');
 
